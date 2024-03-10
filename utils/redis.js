33d9d1058fb3975e0e65b47d0e9d98c0a0dbc6de
Tsegaye16 <abewatsegaye16@gmail.com>
2024-03-10 20:27:20 +0000

    file manage on alx

---
+++ utils/redis.js
@@ -0,0 +1,44 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+// Defining a methods for commonly used redis commands
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to server: ${error}`);
+    });
+  }
+
+  // the metho that check connection status
+  isAlive() {
+    if (this.client.connected) {
+      return true;
+    }
+    return false;
+  }
+
+  // the methods to get value for given key from redis server
+  async get(key) {
+    const redisGet = promisify(this.client.get).bind(this.client);
+    const value = await redisGet(key);
+    return value;
+  }
+
+  // setting a key value pair to redis server
+  async set(key, value, time) {
+    const redisSet = promisify(this.client.set).bind(this.client);
+    await redisSet(key, value);
+    await this.client.expire(key, time);
+  }
+
+  // deleting a key vale pair from redis server
+  async del(key) {
+    const redisDel = promisify(this.client.del).bind(this.client);
+    await redisDel(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
